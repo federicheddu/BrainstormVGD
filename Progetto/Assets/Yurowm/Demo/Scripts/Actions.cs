86791bdb9d96544792f116dbde687827c0c787be
lucafff <48205885+lucafff@users.noreply.github.com>
2020-04-02 13:29:30 +0200

    animazioni nemico
    il nemico ora ti segue e ti spara senza spararti davvero però

--- Progetto/Assets/Yurowm/Demo/Scripts/Actions.cs
+++ Progetto/Assets/Yurowm/Demo/Scripts/Actions.cs
@@ -1,71 +1,135 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.AI;
 
-[RequireComponent (typeof (Animator))]
-public class Actions : MonoBehaviour {
+[RequireComponent(typeof(Animator))]
+public class Actions : MonoBehaviour
+{
 
-	private Animator animator;
+    private Animator animator;
 
-	const int countOfDamageAnimations = 3;
-	int lastDamageAnimation = -1;
+    public GameObject Player;
 
-	void Awake () {
-		animator = GetComponent<Animator> ();
-	}
+    private NavMeshAgent _navMeshAgent;
 
-	public void Stay () {
-		animator.SetBool("Aiming", false);
-		animator.SetFloat ("Speed", 0f);
-		}
+    public float AttackDistance = 10.0f;
+    public float FollowDistance = 12.0f;
+    public float AttackProbability = 0.5f;
 
-	public void Walk () {
-		animator.SetBool("Aiming", false);
-		animator.SetFloat ("Speed", 0.5f);
-	}
+    public AudioClip GunSound = null;
 
-	public void Run () {
-		animator.SetBool("Aiming", false);
-		animator.SetFloat ("Speed", 1f);
-	}
+    const int countOfDamageAnimations = 3;
+    int lastDamageAnimation = -1;
 
-	public void Attack () {
-		Aiming ();
-		animator.SetTrigger ("Attack");
-	}
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
 
-	public void Death () {
-		if (animator.GetCurrentAnimatorStateInfo (0).IsName ("Death"))
-			animator.Play("Idle", 0);
-		else
-			animator.SetTrigger ("Death");
-	}
+    void Update()
+    {
+        if (_navMeshAgent.enabled)
+        {
+            float dist = Vector3.Distance(Player.transform.position, this.transform.position);
+            bool shoot = false;
+            bool follow = (dist < FollowDistance);
 
-	public void Damage () {
-		if (animator.GetCurrentAnimatorStateInfo (0).IsName ("Death")) return;
-		int id = Random.Range(0, countOfDamageAnimations);
-		if (countOfDamageAnimations > 1)
-			while (id == lastDamageAnimation)
-				id = Random.Range(0, countOfDamageAnimations);
-		lastDamageAnimation = id;
-		animator.SetInteger ("DamageID", id);
-		animator.SetTrigger ("Damage");
-	}
+            if (follow)
+            {
+                float random = Random.Range(0.0f, 1.0f);
+                if (random > (1.0f - AttackProbability) && dist < AttackDistance)
+                {
+                    shoot = true;
+                }
+            }
+            Vector3 mov = Vector3.zero;//
+            if (follow)
+            {
+                _navMeshAgent.SetDestination(mov);// Player.transform.position);
+            }
 
-	public void Jump () {
-		animator.SetBool ("Squat", false);
-		animator.SetFloat ("Speed", 0f);
-		animator.SetBool("Aiming", false);
-		animator.SetTrigger ("Jump");
-	}
+            if (!follow || shoot)
+                _navMeshAgent.SetDestination(transform.position);
 
-	public void Aiming () {
-		animator.SetBool ("Squat", false);
-		animator.SetFloat ("Speed", 0f);
-		animator.SetBool("Aiming", true);
-	}
+            if (follow)
+            {
+                Walk();
+            }
+            if (shoot)
+            {
+                Attack();
+            }
 
-	public void Sitting () {
-		animator.SetBool ("Squat", !animator.GetBool("Squat"));
-		animator.SetBool("Aiming", false);
-	}
+            //animator.SetBool("Shoot", shoot);
+            //animator.SetBool("Walk", follow);
+
+        }
+    }
+
+
+    public void Stay()
+    {
+        animator.SetBool("Aiming", false);
+        animator.SetFloat("Speed", 0f);
+    }
+
+    public void Walk()
+    {
+        animator.SetBool("Aiming", false);
+        animator.SetFloat("Speed", 0.5f);
+    }
+
+    public void Run()
+    {
+        animator.SetBool("Aiming", false);
+        animator.SetFloat("Speed", 1f);
+    }
+
+    public void Attack()
+    {
+        Aiming();
+        animator.SetTrigger("Attack");
+    }
+
+    public void Death()
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Death"))
+            animator.Play("Idle", 0);
+        else
+            animator.SetTrigger("Death");
+    }
+
+    public void Damage()
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Death")) return;
+        int id = Random.Range(0, countOfDamageAnimations);
+        if (countOfDamageAnimations > 1)
+            while (id == lastDamageAnimation)
+                id = Random.Range(0, countOfDamageAnimations);
+        lastDamageAnimation = id;
+        animator.SetInteger("DamageID", id);
+        animator.SetTrigger("Damage");
+    }
+
+    public void Jump()
+    {
+        animator.SetBool("Squat", false);
+        animator.SetFloat("Speed", 0f);
+        animator.SetBool("Aiming", false);
+        animator.SetTrigger("Jump");
+    }
+
+    public void Aiming()
+    {
+        animator.SetBool("Squat", false);
+        animator.SetFloat("Speed", 0f);
+        animator.SetBool("Aiming", true);
+    }
+
+    public void Sitting()
+    {
+        animator.SetBool("Squat", !animator.GetBool("Squat"));
+        animator.SetBool("Aiming", false);
+    }
 }
