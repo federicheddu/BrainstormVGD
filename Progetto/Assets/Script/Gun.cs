52577a6ac50e9b0677d172303798728153c9e0c8
Federico Meloni <39567979+federicheddu@users.noreply.github.com>
2020-05-20 19:59:54 +0200

    Nuova pistola e sparo
    Ci sono da aggiungere gli effetti particellari

---
+++ Progetto/Assets/Script/Gun.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+
+    public Camera camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+            Shoot();
+    }
+
+    void Shoot() {
+        RaycastHit hit;
+        if(Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, range)) {
+
+            Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+                target.TakeDamage(damage);
+
+        }
+    }
+}
---
+++ Progetto/Assets/Script/Target.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+
+    public float health = 50f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        if (health <= 0f)
+            Die();
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
