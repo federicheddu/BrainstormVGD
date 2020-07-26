5b09c298f51ae9c3068ff86bf7c1dee7f9c091f4
TurnuAlessandro <48202020+TurnuAlessandro@users.noreply.github.com>
2020-07-26 14:27:28 +0200

    Tolto script che aumentava la vita per il test, aggiunto script per avvisinarsi al giocatore e bloccarsi quando si arriva a una certa distanza

---
+++ Progetto/Assets/InitialMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitialMovement : MonoBehaviour
+{
+    public Transform playerTransform;
+    public float moveSpeed = 2f;
+    public float rotationSpeed = 2f;
+    public float stopDistance = 4f;
+    private Transform myTransform;
+
+    void Awake()
+    {
+        myTransform = transform;
+    }
+
+
+    void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // https://answers.unity.com/questions/353675/how-to-stop-enemy-within-certain-distance-of-playe.html
+    void Update()
+    {
+
+
+        Debug.DrawLine(playerTransform.position, myTransform.position, Color.red);
+        float dist = Vector3.Distance(playerTransform.position, transform.position);
+        
+        // get the target direction:
+        Vector3 targetDir = playerTransform.position - myTransform.position;
+        targetDir.y = 0; // kill any height difference to avoid tilting
+        myTransform.rotation = Quaternion.Slerp(myTransform.rotation, Quaternion.LookRotation(targetDir), rotationSpeed * Time.deltaTime);
+        if (dist > stopDistance)
+        {
+            myTransform.position += myTransform.forward * moveSpeed * Time.deltaTime;
+        }
+        else
+        {
+            // Replace with code to do when character stops
+        }
+        
+    }
+
+}
