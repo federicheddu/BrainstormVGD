60989308fe5774b3ae4f0a0645fa022873f14530
TurnuAlessandro <48202020+TurnuAlessandro@users.noreply.github.com>
2020-08-18 09:52:32 +0200

    Aggiunto visualizzatore di vita per i boss (to fix)

---
+++ Progetto/Assets/CovidHealthBardImageChanger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CovidHealthBardImageChanger : MonoBehaviour
+{
+    public GameObject[] bosses;
+    private Image image;
+    // Start is called before the first frame update
+    void Start()
+    {
+        image = GetComponent<Image>();
+        StartCoroutine(CheckBossesDeath());
+    }
+
+    IEnumerator CheckBossesDeath()
+    {
+        yield return new WaitUntil(() => bosses[0] != null || bosses[1] != null);
+        image.sprite = Resources.Load<Sprite>("one_virus_sprite");
+    }
+}
