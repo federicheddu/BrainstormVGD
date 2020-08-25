68b7cd61220c718228b5875971b7b947bafac7ed
Federico Meloni <39567979+federicheddu@users.noreply.github.com>
2020-08-25 17:06:29 +0200

    salvataggio

---
+++ Progetto/Assets/Scripts/GameData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class GameData
+{
+
+    public int level;
+    public int checkpoint;
+    public int weapon;
+
+    public GameData(int Level, int Checkpoint, int Weapon)
+    {
+        level = Level;
+        checkpoint = Checkpoint;
+        weapon = Weapon;
+    }
+}
---
+++ Progetto/Assets/Scripts/SaveSystem.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using Boo.Lang;
+
+public static class SaveSystem
+{
+    public static void SaveGames(GameData gameData, int slot)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/gameData" + slot + ".vgd";
+
+        FileStream stream = new FileStream(path, FileMode.OpenOrCreate);
+        formatter.Serialize(stream, gameData);
+        stream.Close();
+    }
+
+    public static GameData LoadGame(int slot)
+    {
+        string path = Application.persistentDataPath + "/gameData" + slot + ".vgd";
+        
+        if(File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            GameData data = formatter.Deserialize(stream) as GameData;
+            stream.Close();
+
+            return data;
+
+        } else
+        {
+            Debug.LogError("File di salvataggio non trovato in " + path);
+            return null;
+        }
+    }
+
+    public static GameData[] LoadGames()
+    {
+        GameData[] games = new GameData[6];
+
+        for(int slot = 0; slot<6; slot++)
+        {
+            string path = Application.persistentDataPath + "gameData" + slot + ".vgd";
+
+            if (File.Exists(path))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                FileStream stream = new FileStream(path, FileMode.Open);
+
+                GameData data = formatter.Deserialize(stream) as GameData;
+                stream.Close();
+
+                games[slot] = data;
+
+            }
+            else
+            {
+                GameData data = new GameData(0, 0, 0);
+                games[slot] = data;
+            }
+        }
+
+        return games;
+    }
+}
