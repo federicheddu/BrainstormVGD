d2f8de65adb6cc8c1ec0a5a97b845f682bf2e2e9
TurnuAlessandro <48202020+TurnuAlessandro@users.noreply.github.com>
2020-08-31 02:11:55 +0200

    fix cambi scena dal menù iniziale. fix power up ui anchor

--- Progetto/Assets/Scripts/StartMenu.cs
+++ Progetto/Assets/Scripts/StartMenu.cs
@@ -89,18 +89,8 @@ public class StartMenu : MonoBehaviour
     private void LoadScene(int index)
     {
         Time.timeScale = 1f;
+        //quaaa
         SceneManager.LoadScene(index);
-        if(index == 2)
-        {
-            AudioManager.instance.Play("MusicLiv1");
-        }
-        else if(index == 4)
-        {
-            AudioManager.instance.Play("MusicLiv2");
-        }
-        else if (index == 6)
-        {
-            AudioManager.instance.Play("MusicLiv3");
-        }
+        
     }
 }
